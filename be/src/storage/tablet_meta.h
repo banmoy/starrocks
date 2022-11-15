db79efc189a17dc3e317f7e34c7a8ff5a449cf68
PengFei Li <lpengfei2016@gmail.com>
2022-11-15 22:25:22 +0800

    Add binlog config to tablet meta

--- be/src/storage/tablet_meta.h
+++ be/src/storage/tablet_meta.h
@@ -30,6 +30,7 @@
 #include "common/logging.h"
 #include "common/status.h"
 #include "gen_cpp/olap_file.pb.h"
+#include "storage/binlog_manager.h"
 #include "storage/delete_handler.h"
 #include "storage/olap_common.h"
 #include "storage/olap_define.h"
@@ -180,6 +181,10 @@ public:
         _enable_persistent_index = enable_persistent_index;
     }
 
+    std::shared_ptr<BinlogConfig> get_binlog_config() { return _binlog_config; }
+
+    void set_binlog_config(const BinlogConfig& binlog_config);
+
 private:
     int64_t _mem_usage() const { return sizeof(TabletMeta); }
 
@@ -222,6 +227,8 @@ private:
     // A reference to TabletUpdates, so update related meta
     // can be serialized with tablet meta automatically
     TabletUpdates* _updates = nullptr;
+
+    std::shared_ptr<BinlogConfig> _binlog_config;
 
     std::shared_mutex _meta_lock;
 };
@@ -316,6 +323,18 @@ inline const std::vector<RowsetMetaSharedPtr>& TabletMeta::all_inc_rs_metas() co
 
 inline const std::vector<RowsetMetaSharedPtr>& TabletMeta::all_stale_rs_metas() const {
     return _stale_rs_metas;
+}
+
+void TabletMeta::set_binlog_config(const BinlogConfig& binlog_config) {
+    if (_binlog_config == nullptr) {
+        _binlog_config = std::make_shared<BinlogConfig>();
+    } else if (_binlog_config->version > binlog_config.version) {
+        LOG(WARNING) << "skip to update binlog config, "
+                     << "current version is " << _binlog_config->version << ", update version is "
+                     << binlog_config.version;
+        return;
+    }
+    _binlog_config->update(binlog_config);
 }
 
 // Only for unit test now.
