b02bf9d56ad475363efd439519be54178de88cb2
PengFei Li <lpengfei2016@gmail.com>
2023-01-08 12:39:22 +0800

    add binlog_util.cpp

--- be/src/storage/binlog_util.h
+++ be/src/storage/binlog_util.h
@@ -16,12 +16,10 @@
 
 #include <re2/re2.h>
 
-#include "fs/fs_util.h"
 #include "gen_cpp/binlog.pb.h"
-#include "gutil/strings/numbers.h"
 #include "gutil/strings/substitute.h"
-#include "storage/rowset/rowset.h"
 #include "storage/olap_common.h"
+#include "storage/rowset/rowset.h"
 #include "storage/uint24.h"
 
 namespace starrocks {
@@ -49,34 +47,9 @@ public:
         pb->set_lo(rowset_id.lo);
     }
 
-    static bool get_file_id_from_name(const std::string& file_name, int64_t* file_id) {
-        static re2::RE2 re(R"((\d+)\.binlog)", re2::RE2::Quiet);
-        std::string fild_id_str;
-        bool ret = RE2::PartialMatch(file_name, re, &fild_id_str);
-        if (!ret) {
-            return false;
-        }
+    static bool get_file_id_from_name(const std::string& file_name, int64_t* file_id);
 
-        ret = safe_strto64(fild_id_str, file_id);
-        if (!ret) {
-            LOG(WARNING) << "Invalid binlog file name, file_name: " << file_name << ", file_id_str: " << fild_id_str;
-            return false;
-        }
-        return true;
-    }
-
-    static Status list_binlog_file_ids(std::string& binlog_dir, std::set<int64_t>* binlog_file_ids) {
-        std::set<std::string> file_names;
-        RETURN_IF_ERROR(fs::list_dirs_files(binlog_dir, nullptr, &file_names));
-        int64_t file_id;
-        for (auto& name : file_names) {
-            bool ret = get_file_id_from_name(name, &file_id);
-            if (ret) {
-                binlog_file_ids->emplace(file_id);
-            }
-        }
-        return Status::OK();
-    }
+    static Status list_binlog_file_ids(std::string& binlog_dir, std::set<int64_t>* binlog_file_ids);
 };
 
 } // namespace starrocks
