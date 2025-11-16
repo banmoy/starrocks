316da117da07bb7615f0a5d39a9a31d44e45e7a2
PengFei Li <lpengfei2016@gmail.com>
2025-11-16 10:31:47 +0800

    fix compile
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- be/src/storage/lake/tablet_manager.h
+++ be/src/storage/lake/tablet_manager.h
@@ -142,7 +142,7 @@ public:
 
     StatusOr<TxnLogPtr> get_txn_vlog(const std::string& path, bool fill_cache = true);
 
-    StatusOr<TabletSchemaPtr> get_output_rowset_schema(std::vector<uint32_t>& input_rowset,
+    StatusOr<TabletSchemaPtr> get_output_rowset_schema(const std::vector<uint32_t>& input_rowset,
                                                        const TabletMetadata* metadata);
 
 #ifdef USE_STAROS
